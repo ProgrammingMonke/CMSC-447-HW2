5547cd0edfbc159c9643e515ed7a24669317f14f
Franco <francomaxey@msn.com>
2023-03-13 22:56:58 -0400

    trying to get sqlite3 server to find css and js

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # CMSC-447-HW2
 INCLUDE INFO ON HOW TO RUN THE APPLICATION
+
+npm install
+npm install express
+npm install http
+npm install sqlite3
+node app.js
+
+
+control + shift + p -> SQLite: Open Database -> Choose your database -> open "SQLITE EXPLORER" on bottom left of screen -> look for your table -> right click table -> "Show table"
--- app.js
+++ app.js
@@ -1,35 +1,32 @@
-const sqlite3 = require('sqlite3').verbose();
-let sql;
-
 const express = require("express");
 const app = express();
 var path = require('path');
 var public = path.join(__dirname, 'client_side');
 app.use(express.json());
 
-app.get('/',function(req,res){
-    res.sendFile(path.join(public,'index.html'));
-});
-app.get('/',function(req,res){
-    res.sendFile(path.join(public,'style.css'));
-});
-app.get('/',function(req,res){
-    res.sendFile(path.join(public,'script.js'));
-});
+const sqlite3 = require('sqlite3').verbose();
+let sql;
 
 const http = require('http');
-const port = process.env.PORT || 3030;
-
-
+const port = 1234;
 
 // Connect to DB
 const db = new sqlite3.Database('./data.db',sqlite3.OPEN_READWRITE,(err)=>{
     if(err) return console.error(err.message);
 });
 
-// Create table (do ONCE)
-// sql = 'CREATE TABLE people(name,id,points)';
-// db.run(sql);
+app.get('/',function(req,res){
+    res.sendFile(path.join(public,'index.html'));
+});
+
+app.get('/style.css',function(req,res){
+    res.sendFile(path.join(public,'style.css'),{headers:{'Content-Type': 'text/css'}});
+});
+
+
+// Create table (only if not already existing)
+sql = 'CREATE TABLE IF NOT EXISTS people(name,id,points)';
+db.run(sql);
 
 // Drop table (delete) 
 // db.run("DROP TABLE people");
